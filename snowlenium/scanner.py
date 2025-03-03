60d3f7c2db64dba4f54be2489aba5a6ae0bafbd7
bobllor <bobllor@yahoo.com>
2025-03-03 15:12:03 -0600

    Refactored code

--- snowlenium/scanner.py
+++ snowlenium/scanner.py
@@ -3,6 +3,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from typing import Iterable
 
 class VTBScanner(Driver):
     def __init__(self, driver):
@@ -43,7 +44,7 @@ class VTBScanner(Driver):
         
         return ritm_elements
     
-    def get_element(self, search_val: str, xpath_str = str) -> WebElement | None:
+    def get_element(self, search_val: str, xpaths = Iterable[str]) -> WebElement | None:
         '''Returns a WebElement that contains the matching text value located in the container.
         If not found, None is returned.
 
@@ -55,14 +56,22 @@ class VTBScanner(Driver):
                 Text that can be found in the card container on the VTB. The elements returned from
                 this method searches elements that contain the text value.
 
-            xpath_str: str
-                The locator value that is used to search for. This must be in a format of a
-                relative path and a **XPATH**. For example, `//li[@foo="0" and @bar="0"]//a`,
-                where the search is performed inside the `<a>` element.
+            xpaths: Iterable[str]
+                Any ordered iterable that contains the locator value used to search for an element.
+                The iterable must be **non-zero**, and can contain any amount of locator values. If more than
+                one locator value is passed, the method assumes this is a nested element and will navigate it
+                accordingly. This must be in a format of a relative path and a **XPATH**.
+                
+                For example, [`'//li[@foo="0" and @bar="0"]', '//a'`], will find the matching `<li>` element,
+                then the `<a>` element in the `<li>` element where it performs the search for the `search_val`.
         '''
+        if len(xpaths) < 0:
+            raise ValueError(f'Cannot have an empty iterable.')
+
         try:
-            ritm_element = self.presence_find_element(
-                f'{xpath_str}[contains(text(), "{search_val}")]', by=By.XPATH)
+            for path in xpaths:
+                ritm_element = self.presence_find_element(
+                    f'{path}[contains(text(), "{search_val}")]', by=By.XPATH)
         except TimeoutException:
             return None
         
