802f322bde0de265a5105c36d9d061c0583b5f50
bobllor <bobllor@yahoo.com>
2025-03-10 15:24:14 -0500

    Comments & name changes, small bug fix

--- snowlenium/scanner.py
+++ snowlenium/scanner.py
@@ -3,29 +3,20 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from typing import Iterable
 
 class VTBScanner(Driver):
     def __init__(self, driver):
         '''Class used to scan for items on the Virtual Task Board (VTB) of Service Now.
-
-        This class assumes that the driver is on the correct URL.
-        
-        Parameters
-        ----------
-            lane_xpath: str
-                The HTML xpath of a lane in the VTB. 
         '''
         super().__init__(driver)
     
-    def get_elements(self, search_val: str, xpaths: Iterable[str]) -> list[WebElement] | list:
-        '''Returns a list of WebElements on the VTB. If none found, an empty list is returned.
+    def get_elements(self, search_val: str, locators: list[str]) -> list[WebElement] | list:
+        '''Scans the VTB and returns a list of WebElements.
 
-        The method searches for the elements based on the value of `search_val`, and returns filtered WebElements
-        containing `search_val`. It uses the XML function `contains()` to get the result, which is **case sensitive**.
-
-        If attempting to find elements in a certain lane, the ending xpath **must be a relative xpath**.
-        If an absolute xpath is used, then the search will occur for the entire page.
+        Searches for the elements based on the value of `search_val`, and returns WebElements that contains
+        `search_val`. It uses the XML function `contains()` to get the result.
+        If attempting to find elements in a certain swim lane, the final xpath in the list **must be a relative path**.
+        Absolute paths will ignore the hierarchy of the DOM and will attempt to search for all matching tags.
         
         Parameters
         ----------
@@ -33,20 +24,21 @@ class VTBScanner(Driver):
                 Text that can be found in the card container on the VTB. The elements returned from
                 this method searches elements that contain the text value.
 
-            xpath_str: str
+            locators: str
                 The locator value that is used to search for. This must be in a format of a
-                relative path and a **XPATH**. For example, `//li[@foo="0" and @bar="0"]//a`,
-                where the search is performed inside the `<a>` element.
+                relative path and a **XPATH**. 
+        ```
+
         '''
-        elements = self._iterate_element_array(By.XPATH, xpaths[:-1])
+        elements = self._traverse_elements(By.XPATH, locators[:-1])
         
         if elements != None:
             elements = elements.find_elements(By.XPATH,
-                f'{xpaths[-1]}[contains(text(), "{search_val}")]')
+                f'{locators[-1]}[contains(text(), "{search_val}")]')
         
-        return elements
+        return elements if not None else []
     
-    def get_element(self, search_val: str, xpaths = Iterable[str]) -> WebElement | None:
+    def get_element(self, search_val: str, locators = list[str]) -> WebElement | None:
         '''Returns a WebElement that contains the matching text value located in the container.
         If not found, None is returned.
 
@@ -58,29 +50,29 @@ class VTBScanner(Driver):
                 Text that can be found in the card container on the VTB. The elements returned from
                 this method searches elements that contain the text value.
 
-            xpaths: Iterable[str]
-                Any ordered iterable that contains the locator value used to search for an element.
-                The iterable must be **non-zero**, and can contain any amount of locator values. If more than
+            locators: list[str]
+                A list that contains the locator value used to search for an element.
+                The list must be **non-zero**, and can contain any amount of locator values. If more than
                 one locator value is passed, the method assumes this is a nested element and will navigate it
                 accordingly. This must be in a format of a relative path and a **XPATH**.
                 
                 For example, [`'//li[@foo="0" and @bar="0"]', '//a'`], will find the matching `<li>` element,
                 then the `<a>` element in the `<li>` element where it performs the search for the `search_val`.
         '''
-        if len(xpaths) < 0:
+        if len(locators) < 0:
             raise ValueError(f'Cannot have an empty iterable.')
         
-        element = self._iterate_element_array(By.XPATH, xpaths[:-1])
+        element = self._traverse_elements(By.XPATH, locators[:-1])
 
         if element != None:
             try:
                 element = element.find_element(By.XPATH,
-                    f'{xpaths[-1]}[contains(text(), "{search_val}")]')
+                    f'{locators[-1]}[contains(text(), "{search_val}")]')
             except NoSuchElementException:
                 return None
             
         return element
 
-    def drag_task(self, search_val: str):
+    def drag_task(self, search_val: str, *, drag_from: str, drag_to: str):
         '''Drags the task over to a desired swim lane on the VTB.'''
         pass
