abd264680cc7a6848d24945e1d04fbd7aa99d1f8
bobllor <bobllor@yahoo.com>
2025-03-19 16:18:53 -0500

    Laid groundwork for new method

--- snowlenium/scraper.py
+++ snowlenium/scraper.py
@@ -6,8 +6,12 @@ class Scraper(Driver):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def get_element_attribute(self, locators: list[str], by: str = By.XPATH, *, attribute: str = 'value') -> str:
+    def get_element_attribute(self, locators: list[str], by: str = By.XPATH, *, attribute: str = 'value'):
         '''Get the attribute of an HTML element.'''
         web_ele = self._traverse_locators(by, locators)
 
         return web_ele.get_attribute(attribute)
+    
+    def get_all_elements(self, locators: list[str], by: str = By.XPATH):
+        '''Returns a list of elements based on the value of the last index in the list.'''
+        pass
