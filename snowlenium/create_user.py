e6cc41502c2ba675ea7a4f7ac7f00f1af7e0c7f7
bobllor <bobllor@yahoo.com>
2025-03-19 20:15:51 -0500

    New method extracted from fill_fields and groundwork for error checking

--- snowlenium/create_user.py
+++ snowlenium/create_user.py
@@ -12,11 +12,14 @@ class UserCreation(Driver):
     def __init__(self, driver):
         super().__init__(driver)
     
-    def create_user(self, *, values_info: list[tuple[str, str, str]], submit_element: str, sleep_time: float = 0):
-        '''Creates the user to add into the database. When the user is filled out, submit is clicked on.
-        This method assumes that the driver is on the user creation page.
+    def fill_fields(self, *, values_info: list[tuple[str, str, str]], sleep_time: float = 0) -> None:
+        '''Fill the fields of the user on the user creaton page. The driver assumes it is
+        already on the user creation page.
 
-        It is important to pass the DOM elements that the driver will be interacting with.
+        This method **does not** submit an entry. Invoke the `submit` method to submit an entry.
+        
+        Error handling on the user creation page is not checked here. 
+        Use the method `check_errors` to handle errors on the page.
         
         Parameters
         ----------
@@ -25,13 +28,9 @@ class UserCreation(Driver):
                 VALUE will be inserted into the DOM_ELEMENT by searching the LOCATOR.
                 For example: ('USER_EMAIL', 'user.email', By.ID), will insert
                 'USER_EMAIL' at the DOM element with the ID 'user.email'.
-            
-            submit_element: str
-                The element of the DOM that represents the save/submit button to insert
-                the user into the database.
 
             sleep_time: float
-                A float used to delay each key insertion to the DOM element. By default it has a
+                A float used to delay each key insertion to the DOM element. By default it has
                 no delay.
         '''
         for tup in values_info:
@@ -41,3 +40,38 @@ class UserCreation(Driver):
                 element.send_keys(tup[0])
 
             time.sleep(sleep_time)
+
+    def submit(self, submit_dom: str, *, locator: str = 'id') -> None:
+        '''Submits the current entry on the user creation page.
+        
+        Parameters
+        ----------
+            submit_dom: str
+                The DOM of the element that represents the save/submit button to
+                insert the user into the database.
+
+            by: str
+                The locator used to find the element. By default it is By.ID.
+        '''
+        submit_element = self.presence_find_element(submit_dom, by=locator)
+
+        submit_element.click()
+
+    def check_errors(self, error_doms: list[str]) -> list[str]:
+        '''Checks the page for errors and return a list of strings representing
+        the text of the error messages.
+        
+        Parameters
+        ----------
+            error_doms: list[str]
+                A list of DOM elements that represents the errors.
+        '''
+        errors = []
+
+        for dom in error_doms:
+            error_element: WebElement = self.presence_find_element(dom, By.CSS_SELECTOR)
+
+            if error_element is not None:
+                errors.append(error_element.text)
+
+        return errors
