9a80f685bef97767ed77dafd3101ca4fb2a44c62
bobllor <bobllor@yahoo.com>
2025-03-22 21:38:38 -0500

    Fixed and added new shadow root methods, docstring fixes

--- snowlenium/driver.py
+++ snowlenium/driver.py
@@ -4,9 +4,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.shadowroot import ShadowRoot
-from selenium.common.exceptions import TimeoutException, NoSuchFrameException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException, NoSuchFrameException
 from selenium.webdriver.common.action_chains import ActionChains
-from typing import Iterable
 import selenium.webdriver.chrome.webdriver as chrome
 import selenium.webdriver.firefox.webdriver as firefox
 from selenium.webdriver.chrome.options import Options as chromeOptions
@@ -40,34 +39,34 @@ class Driver:
         self.driver_wait = WebDriverWait(self.driver, self.wait_time)
         self.action_driver = ActionChains(self.driver)
         
-    def set_wait_timer(self, value: int = 6) -> None:
-        '''Sets the wait timer for `WebDriverWait` to a given value. By default it is 6 seconds.'''
+    def set_wait_timer(self, value: float | int = 6) -> None:
+        '''Sets the wait timer for `WebDriverWait` to a given value. 
+        Setting the wait timer will effect the time it takes to look for an element.
+        By default it is 6 seconds.'''
         self.wait_time = value
+
+        self.driver_wait = WebDriverWait(self.driver, self.wait_time)
     
     def go_to(self, url: str) -> None:
-        '''Goes to the given URL argument.'''
+        '''Sends the driver to a URL.'''
         if not isinstance(url, str):
             raise TypeError(f'Expected url to be type str but got {type(url)}')
         
         self.driver.get(url)
     
-    def switch_frames(self, frame_name: str = 'gsft_main', *, return_default: bool = True):
+    def switch_frames(self, frame_name: str | WebElement = 'gsft_main', *, return_default: bool = True):
         '''Switch frames on the current page. If the frame isn't found, it will remain on the default frame
         of the page.
 
-        This method does not account for shadow roots inside the
-
-        For more fine control over frame switching, use the built-in WebDriver method `switch_to`.
-
         Parameters
         ----------
-            frame_name: str 
-                A string that represents the frame ID attribute of the current page.
+            frame_name: str | WebElement
+                The name of the frame to switch to, it can be a string or a WebElement.
                 By default the value is `gsft_main`.
 
             return_default: bool 
                 Switch the drive back to the default frame of the page before switching to a new frame.
-                Ensures that there is no frame before interacting with a frame. Default is `True`.
+                Default is `True`.
         '''
         if return_default:
             self.driver.switch_to.default_content()
@@ -81,26 +80,20 @@ class Driver:
             self.driver.switch_to.default_content()
     
     def switch_default_frame(self):
-        '''Returns back to the default frame.'''
+        '''Returns the driver back to the default frame.'''
         self.driver.switch_to.default_content()
     
-    def navigate_shadow_root(self, locator: str = By.CSS_SELECTOR, *, html_elements: Iterable[str] = None) -> ShadowRoot:
-        '''Returns a ShadowRoot of the last element in any iterable structure.
-
-        Navigating a DOM with shadow roots is different from directly accessing a HTML element.
-        The elements inside the `html_elements` iterable must have a `#shadow-root` as its child, 
-        otherwise `NoSuchElementException` is thrown.
-
-        `html_elements` must be a minimum size 1.
+    def get_shadowroot_element(self, locator: str | By = By.CSS_SELECTOR, *, html_elements: list[str] = None) -> ShadowRoot:
+        '''Returns a ShadowRoot of the last element in the list.
         
         Parameters
         ----------
-            by: str
+            by: str | By
                 Locator strategy, can use the literal string equivalent or the By strategy. 
                 By default it locates by `css selector`.
 
-            html_elements: Iterable[str]
-                Any ordered iterable structure containing HTML elements that contains a shadow root.
+            html_elements: list[str]
+                Any list structure containing HTML elements that contains a shadow root.
         '''
         if not any(isinstance(element, str) for element in html_elements):
             raise TypeError(f'Got unexpected type in html_elements')
@@ -115,14 +108,44 @@ class Driver:
                 sr = sr.find_element(locator, s_root).shadow_root
 
         return sr
+    
+    def navigate_shadowroot(self, locator: str | By = By.CSS_SELECTOR, html_elements: list[str] = None):
+        '''Navigates the driver into a ShadowRoot that contains an iframe. This methods assumes the last
+        element in the list will have an iframe.
+
+        If attempting to search in a ShadowRoot, it is better to use `get_shadowroot_element` for the
+        ShadowRoot. Shadow roots do not have an ID for the driver to switch focus to.
+        
+        Parameters
+        ----------
+            by: str | By
+                Locator strategy, can use the literal string equivalent or the By strategy. 
+                By default it locates by `css selector`.
+
+            html_elements: list[str]
+                Any list structure containing HTML elements that contains a shadow root.
+        '''
+        if not all(isinstance(element, str) for element in html_elements):
+            raise TypeError(f'Got unexpected type in html_elements')
+
+        if len(html_elements) < 1:
+            raise ValueError(f'Cannot have an empty iterable, got {len(html_elements)} size')
+        
+        sr = self.driver.find_element(locator, html_elements[0]).shadow_root
+        
+        if len(html_elements) > 1:
+            for s_root in html_elements[1:]:
+                sr = sr.find_element(locator, s_root).shadow_root
+
+        self.switch_frames(sr.find_element(By.CSS_SELECTOR, 'iframe'))
             
-    def presence_find_element(self, locator=By.ID, value: str = None) -> WebElement:
+    def presence_find_element(self, locator: str | By = By.ID, value: str = None) -> WebElement:
         '''Return a `WebElement` by using an expected condition and `WebDriverWait`. 
         If no element is found, a `TimeoutException` exception is raised.
         
         Parameters
         ----------
-            by: str
+            by: str | By
                 Locator strategy, can use the literal string equivalent or the By strategy. 
                 By default it locates by `id`.
 
@@ -137,10 +160,15 @@ class Driver:
         ))
         
         return ele
+
+    def find_elements(self, locator: str | By, value: str) -> list[WebElement]:
+        '''Returns a list of WebElements containing all elements matching the value.
+        If none found, then an empty list is returned.'''
+        return self.driver.find_elements(locator, value)
     
-    def _traverse_html_elements(self, locator: str, html_elements: Iterable[str]) -> WebElement:
-        '''Iterate an iterable structure and return the associated WebElement.
-        
+    def _traverse_html_elements(self, locator: str | By, html_elements: list[str]) -> WebElement:
+        '''Iterate through a list and return the associated WebElement.
+    
         If not found, a `NoSuchElementException` exception is raised.
         '''
         element = self.presence_find_element(locator, value=html_elements[0])
