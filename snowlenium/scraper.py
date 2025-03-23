5cb7e91d730742dc78e4a47eb9f6535367ab8026
bobllor <bobllor@yahoo.com>
2025-03-22 22:03:52 -0500

    Updated docstring and parameters

--- snowlenium/scraper.py
+++ snowlenium/scraper.py
@@ -7,13 +7,27 @@ class Scraper(Driver):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def get_element_attribute(self, locator: str, html_elements: list[str], *, attribute: str = 'value') -> str:
-        '''Get the attribute of an HTML element.'''
+    def get_element_attribute(self, locator: str | By, html_elements: list[str], *, attribute: str = 'value') -> str:
+        '''Get the attribute of an HTML element.
+        
+        Parameters
+        ----------
+            locator: str | By
+                The locator used to search for the element.
+
+            html_elements: list[str]
+                A list of HTML elements used to get an attribute of an element. The last element in the list
+                is where the search is being performed on. If more than one element is given, the method
+                assumes it is a nested element and navigates the tree accordingly.
+
+            attribute: str
+                The HTML attribute to get the value from. By default is gets from the `value` attribute.
+        '''
         web_ele = self._traverse_html_elements(locator, html_elements)
 
         return web_ele.get_attribute(attribute)
     
-    def get_all_elements(self, locator: str, html_elements: list[str]) -> list[WebElement]:
+    def get_all_elements(self, locator: str | By, html_elements: list[str]) -> list[WebElement]:
         '''Returns a list of WebElements based on the value of the last index in the list.'''
         if len(html_elements) > 1:
             traversed_element = self._traverse_html_elements(locator, html_elements)
