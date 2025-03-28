d0d4ecfa3fcb9ba19d6d91ec7a4683d94faba4cf
bobllor <bobllor@yahoo.com>
2025-03-28 09:04:59 -0500

    Changed from closure to regular function

--- librelnium/js_scripts/scroll-utils/find-scrollable-element.js
+++ librelnium/js_scripts/scroll-utils/find-scrollable-element.js
@@ -1,22 +1,20 @@
-window.findScrollableElement = (query = null) => {
+window.findScrollableElement = (query = null, property) => {
     let queryType = query === null ? 'div' : query;
 
     let elements = document.querySelectorAll(queryType);
     const scrollStyles = new Set(['auto', 'scroll']);
 
-    return function (property){
-        for(let i = 0; i < elements.length - 1; i++){
-            let styles = window.getComputedStyle(elements[i]);
-    
-            let elementStyle = styles.getPropertyValue(property);
-    
-            let isScrollable = elements[i].scrollHeight > elements[i].clientHeight;
-    
-            if(scrollStyles.has(elementStyle) && isScrollable){
-                return elements[i];
-            }
-        }
+    for(let i = 0; i < elements.length - 1; i++){
+        let styles = window.getComputedStyle(elements[i]);
 
-        return null;
+        let elementStyle = styles.getPropertyValue(property);
+
+        let isScrollable = elements[i].scrollHeight > elements[i].clientHeight;
+
+        if(scrollStyles.has(elementStyle) && isScrollable){
+            return elements[i];
+        }
     }
+
+    return null;
 }
